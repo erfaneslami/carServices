0dd84ba8debba09e825e42de8aaab202eddf6e3a
erfan <erfan.88.eslami@gmail.com>
2022-05-22 12:02:53 -0700

    add car card

--- src/Components/Home/CarCard.js
+++ src/Components/Home/CarCard.js
@@ -12,16 +12,16 @@ import divaf from "../../Assets/img/207.png";
 const CarCard = () => {
   return (
     <Card>
-      <CardActionArea sx={{ display: "flex", justifyContent: "space-between" }}>
+      <CardActionArea sx={{ display: "flex", justifyContent: "space-around" }}>
         <CardContent>
-          <Typography variant="h3">PEUGEOT</Typography>
-          <Typography variant="h4">207</Typography>
+          <Typography variant="h5">PEUGEOT</Typography>
+          <Typography variant="h6">207</Typography>
           <Typography variant="subtitle1">30,000 KM</Typography>
           <Button variant="contained" sx={{ margin: "2rem 0" }}>
             Detail
           </Button>
         </CardContent>
-        <CardMedia component="img" image={divaf} sx={{ maxWidth: "55%" }} />
+        <CardMedia component="img" image={divaf} sx={{ maxWidth: "50vw" }} />
       </CardActionArea>
     </Card>
   );
