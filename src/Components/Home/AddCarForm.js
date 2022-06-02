c794d1ba98c8c2fb769f7df74331a183cc435db4
erfan eslami <erfan.88.eslami@gmail.com>
2022-06-02 19:12:31 +0430

    add ability to add car and send to databae

--- src/Components/Home/AddCarForm.js
+++ src/Components/Home/AddCarForm.js
@@ -1,9 +1,11 @@
 import { AppBar, Box, Button, Toolbar } from "@mui/material";
 import { Controller, useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import FormInput from "../Auth/FormInput";
-import { useContext } from "react";
 import AuthContext from "../../Store/Auth-context";
+import LoadingSpinner from "../../UI/LoadingSpinner";
+
 const AddCarForm = () => {
   const { control, handleSubmit, formState } = useForm({
     mode: "onBlur",
@@ -15,36 +17,46 @@ const AddCarForm = () => {
   });
   const { errors } = formState;
   const authCtx = useContext(AuthContext);
-  console.log(authCtx);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const onSubmit = async (carData) => {
-    console.log(carData);
-
     const id = (Date.now() * Math.random()).toString().slice(0, 11);
-    const response = fetch(
-      `https://carservices-server-default-rtdb.firebaseio.com/users/${authCtx.localId}/cars/${id}.json`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          carBrand: carData.carBrand,
-          carName: carData.carName,
-          carKM: carData.carKM,
-        }),
-      }
-    );
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://carservices-server-default-rtdb.firebaseio.com/users/${authCtx.localId}/cars/${id}.json`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            carBrand: carData.carBrand,
+            carName: carData.carName,
+            carKM: carData.carKM,
+          }),
+        }
+      );
 
-    const data = (await response).json();
-    console.log(data);
+      const data = await response.json();
+      if (data.error) throw data;
+
+      navigate("/welcome");
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+    setIsLoading(false);
   };
+
   return (
     <Box
       component="form"
       onSubmit={handleSubmit(onSubmit)}
       sx={{ width: "70vw", margin: "0 auto" }}
     >
+      {isLoading && <LoadingSpinner />}
       <Controller
         name="carBrand"
         control={control}
