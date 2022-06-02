c867cdd3bfc824422ecaec064897a0332e9480b8
erfan eslami <erfan.88.eslami@gmail.com>
2022-06-02 18:52:56 +0430

    add auth data to local storage

--- src/Store/AuthContextProvider.js
+++ src/Store/AuthContextProvider.js
@@ -2,21 +2,25 @@ import { useState } from "react";
 import AuthContext from "./Auth-context";
 
 const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialAuth = JSON.parse(localStorage.getItem("userAuth"));
+  console.log(initialAuth);
+  const [token, setToken] = useState(initialAuth?.token);
   const [username, setUsername] = useState(null);
-  const [localId, setLocalId] = useState(null);
+  const [localId, setLocalId] = useState(initialAuth?.localId);
 
   const isLoggedIn = !!token;
 
   const loginHandler = (token, localId) => {
     setToken(token);
     setLocalId(localId);
+    localStorage.setItem("userAuth", JSON.stringify({ token, localId }));
   };
 
   const signupHandler = (token, username, localId) => {
     setToken(token);
     setUsername(username);
     setLocalId(localId);
+    localStorage.setItem("userAuth", JSON.stringify({ token, localId }));
   };
 
   const logoutHandler = () => {
