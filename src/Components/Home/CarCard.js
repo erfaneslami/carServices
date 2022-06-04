9bf62185c5976f7c0af4fb8a3e03620c241d958c
erfan eslami <erfan.88.eslami@gmail.com>
2022-06-04 19:35:58 +0430

    add auto images

--- src/Components/Home/CarCard.js
+++ src/Components/Home/CarCard.js
@@ -8,8 +8,32 @@ import {
 } from "@mui/material";
 
 import towOSeven from "../../Assets/img/207.png";
+import towOSix from "../../Assets/img/206.png";
+import pars from "../../Assets/img/pars.png";
+import bereliance from "../../Assets/img/bereliance.png";
+import tara from "../../Assets/img/tara.png";
+import rana from "../../Assets/img/rana.png";
+import vitara from "../../Assets/img/vitara.png";
+import defaultImg from "../../Assets/img/default.svg";
 
 const CarCard = (props) => {
+  const cover =
+    `${props.carName}`.toLowerCase() === "207"
+      ? towOSeven
+      : `${props.carName}`.toLowerCase() === "206"
+      ? towOSix
+      : `${props.carName}`.toLowerCase() === "pars" ||
+        `${props.carName}`.toLowerCase() === "persia"
+      ? pars
+      : `${props.carName}`.toLowerCase() === "vitara"
+      ? vitara
+      : `${props.carName}`.toLowerCase() === "rana"
+      ? rana
+      : `${props.carName}`.toLowerCase() === "tara"
+      ? tara
+      : `${props.carName}`.toLowerCase() === "bereliance"
+      ? bereliance
+      : defaultImg;
   return (
     <Card>
       <CardActionArea sx={{ display: "flex", justifyContent: "space-around" }}>
@@ -21,11 +45,7 @@ const CarCard = (props) => {
             Detail
           </Button>
         </CardContent>
-        <CardMedia
-          component="img"
-          image={towOSeven}
-          sx={{ maxWidth: "50vw" }}
-        />
+        <CardMedia component="img" image={cover} sx={{ maxWidth: "50vw" }} />
       </CardActionArea>
     </Card>
   );
